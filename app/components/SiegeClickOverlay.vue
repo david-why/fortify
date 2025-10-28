f0ce3cd82781e9f4dab2200b13049d071e7c6787
david-why <david_why@outlook.com>
2025-10-28 16:56:23 +0800

    copy svg code from siege website

---
+++ app/components/SiegeClickOverlay.vue
@@ -0,0 +1,70 @@
+<template>
+  <svg viewBox="0 0 2732 2048" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M1243.5 664.5V577.999C1263.5 569.999 1314.6 553.499 1359 551.499C1403.4 549.499 1462.83 560.999 1487 566.999V659L1458.5 687.5V930.5L1394.5 957L1351 930.5L1326.5 937V979.5L1344.5 996V1114L1321 1119.5L1269.5 1101V687.5L1243.5 664.5Z"
+      fill="#E40000"
+      fill-opacity="0"
+      stroke=""
+      stroke-width="2"
+      cursor="pointer"
+      transform="translate(-15, -290)"
+      style="opacity: 1"
+    ></path>
+    <path
+      d="M1641 884L1568 771.5L1385.5 832V723L1366.5 706.5V667L1388.5 660.5L1434.5 686.5L1498 659.5V601.5L1598.5 642L1619.5 635.5L1663.5 668.5L1672.5 649L1778.5 635.5L1887 656.5V730L1985.5 766V809L1955 819.5V916.5L1894.5 982.5L1887 1088.5L1847.5 1118.5V1145L1769.5 1166L1635 1105L1641 884Z"
+      fill="#C72C2C"
+      fill-opacity="0"
+      stroke=""
+      stroke-width="2"
+      cursor="pointer"
+      transform="translate(-50, -15)"
+      style="opacity: 1"
+    ></path>
+    <path
+      d="M97 66L290 1.5L361.5 113L355.5 332.5L180 404.5L7 324.5L13.5 124L1.5 112L97 66Z"
+      fill="#C72C2C"
+      fill-opacity="0"
+      stroke=""
+      stroke-width="2"
+      cursor="pointer"
+      transform="translate(1225, 760)"
+      style="opacity: 1"
+    ></path>
+    <path
+      d="M129 300L9.5 229.5L13 140L1 129.5L60.5 79.5L226.5 1.5L322.5 100L409 104L398.5 172L359 202.5L315 172V224L294.5 262.5L262 258.5L250 234L129 300Z"
+      fill="#C72C2C"
+      fill-opacity="0"
+      stroke=""
+      stroke-width="2"
+      cursor="pointer"
+      transform="translate(525, 1010)"
+    ></path>
+    <path
+      d="M23 58.5L33.5 22L59.5 13.5L74.5 8.5H89L112.5 13.5L124.5 23.5L144.5 19L150.5 34L170 41.5L203.5 1L271.5 4.5V23.5L364.5 58.5L371.5 41.5L405.5 39L392 70.5L607 153L368.5 277L253 221L250.5 203H220L210.5 215.5L122 227.5L23 208L1 162L10 117L26 102.5L23 58.5Z"
+      fill="#C72C2C"
+      fill-opacity="0"
+      stroke=""
+      stroke-width="2"
+      cursor="pointer"
+      transform="translate(1250, 1320)"
+    ></path>
+    <path
+      d="M95.5 1476L1 1627.5L1033 1629L1028.5 1.5L817 50.5L856.5 263.5L837 733.5L787.5 716.5L770.5 733.5L748.5 805.5L712.5 656L660 589L597 571L528.5 589L495.5 638L489 709.5L472.5 733.5L489 788.5L555.5 805.5L548.5 835.5L510.5 844L489 900L463 912L414 999.5L445.5 1011.5L414 1054.5L388.5 1106.5L395.5 1154L368 1165L358 1145.5L359.5 1106.5L312 1094.5L260.5 1037L246.5 1039.5L215.5 1098L170 1133L128.5 1105L106 1113L95.5 1134.5V1203L77 1258L118.5 1295.5L107 1314.5L164 1350.5L118.5 1430.5L141 1451L95.5 1476Z"
+      fill="#C72C2C"
+      fill-opacity="0"
+      stroke=""
+      stroke-width="2"
+      cursor="pointer"
+      transform="translate(1700, 420)"
+    ></path>
+    <path
+      d="M10 67.5L70.5 1L125 47L113 117.5L77 148L1.5 176L10 67.5Z"
+      fill="#C72C2C"
+      fill-opacity="0"
+      stroke=""
+      stroke-width="2"
+      cursor="pointer"
+      transform="translate(1835, 900)"
+    ></path>
+  </svg>
+</template>
